c574c277633fcb8fc9bcecbd7dfde4d481f0cee1
Julien Huynh <julienhuynh@macbook-pro-de-julien.home>
2023-03-17 11:19:32 +0100

    new repo route

--- backend_symfony/scripts/create_new_repository.sh
+++ backend_symfony/scripts/create_new_repository.sh
@@ -1,7 +1,17 @@
-REPOSITORY_PATH=$1
-REPOSITORY_NAME=$2
-USERNAME=$3
-REPOSITORY_FILES=$4
+REPOSITORY_PATH="/home/lsamuel/repositories"
+REPOSITORY_NAME="index-app"
+USERNAME="alexis"
+REPOSITORY_FILES='
+[
+  {
+    "name": "test.html",
+    "data": "PGgxPkhlbGxvLCBXb3JsZCE8L2gxPg=="
+  },
+  {
+    "name": "script.js",
+    "data": "dmFyIHN0cmluZyA9IHN0cmluZyArICh2YXIgbmFtZSA9ICJIZWxsbyI7IHN0cmluZy5yZXBsYWNlKCJoZWxsb1wiKSAqICR7bmFtZX0pOw=="
+  }
+]'
 
 # Création du dossier utilisateur si n'existe pas
 if [ ! -d "/home/${USERNAME}" ]; then
@@ -12,7 +22,8 @@ fi
 # Création du dossier du repository
 mkdir "${REPOSITORY_PATH}/${REPOSITORY_NAME}"
 
-for file in $(echo REPOSITORY_FILES | jq -r '.files[] | @base64'); do
+# Décoder et enregistrer les fichiers encodés en base64 à partir de la variable JSON
+for file in $(echo "$REPOSITORY_FILES" | jq -r '.[] | @base64'); do
     filename=$(echo "$file" | base64 --decode | jq -r '.name')
     filedata=$(echo "$file" | base64 --decode | jq -r '.data')
     echo "$filedata" | base64 --decode > "${REPOSITORY_PATH}/${REPOSITORY_NAME}/${filename}"
