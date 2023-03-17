8b502d594f58217322458432c52f3f315ba865bd
Lunion.sam <114447916+UnitIgris@users.noreply.github.com>
2023-03-17 16:26:35 +0100

    test delete

--- frontend/src/Hook/useGetDiskConsomation.js
+++
@@ -1,9 +0,0 @@
-export default function useGetDiskConsomation() {
-    return function () {
-        return fetch('http://localhost:1234/bdd-size', {
-            method: 'GET',
-            mode: "cors"
-        })
-            .then(data => data.json())
-    }
-}
